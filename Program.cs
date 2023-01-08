d1c9e2a09426e494413dd2a5678eaef23d1c272c
JezReal <jezreeljoshuam@gmail.com>
2023-01-09 02:21:03 +0800

    configure serializer to use snake case

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Yoh.Text.Json.NamingPolicies;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers().AddJsonOptions(options => {
+    options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicies.SnakeCaseLower;
+});
 
 var app = builder.Build();
 
